536b940b5fa3119632f7a1c90d63146cfbe413f1
kanaru0928 <kanaru0928@gmail.com>
2025-01-04 11:27:38 +0900

    fix: githubのAPIコールを改善

--- remix-app/app/loader/github-contributes.ts
+++ remix-app/app/loader/github-contributes.ts
@@ -1,26 +1,29 @@
 import { TZDate } from "@date-fns/tz";
-import { format, subMonths } from "date-fns";
+import { format, parse, subMonths } from "date-fns";
 import { getGitHubToken } from "./parameters";
 
 export async function getGithubContributesByMonth(
-  year: number,
-  month: number
-): Promise<number> {
-  const nextMonth = month + 1 > 12 ? 1 : month + 1;
-  const nextYear = month + 1 > 12 ? year + 1 : year;
+  dates: { year: number; month: number }[]
+): Promise<Record<string, number>> {
+  const queries = dates.map(({ year, month }) => {
+    const nextMonth = month + 1 > 12 ? 1 : month + 1;
+    const nextYear = month + 1 > 12 ? year + 1 : year;
 
-  const from = new TZDate(year, month - 1, "Asia/Tokyo").toISOString();
-  const to = new TZDate(nextYear, nextMonth - 1, "Asia/Tokyo").toISOString();
+    const from = new TZDate(year, month - 1, "Asia/Tokyo");
+    const to = new TZDate(nextYear, nextMonth - 1, "Asia/Tokyo");
+
+    return `${format(from, "MMMyy")}: contributionsCollection (from: "${from.toISOString()}", to: "${to.toISOString()}") {
+            contributionCalendar {
+              totalContributions
+            }
+          }`;
+  });
 
   const query = {
     query: `
     query {
       user(login: "kanaru0928") {
-        contributionsCollection (from: "${from}", to: "${to}") {
-          contributionCalendar {
-            totalContributions
-          }
-        }
+        ${queries.join("\n")}
       }
     }
     `,
@@ -41,29 +44,33 @@ export async function getGithubContributesByMonth(
   });
   const json = await response.json();
   // console.log(json);
-  return json.data.user.contributionsCollection.contributionCalendar
-    .totalContributions;
+  Object.keys(json.data.user).forEach((key) => {
+    json.data.user[key] =
+      json.data.user[key].contributionCalendar.totalContributions;
+  });
+
+  return json.data.user;
 }
 
 export async function getGithubContributesChart() {
   let date = new Date();
 
-  const promises: Promise<number>[] = [];
-  for (let i = 0; i < 12; i++) {
-    promises.push(
-      getGithubContributesByMonth(date.getFullYear(), date.getMonth() + 1)
-    );
-    date = subMonths(date, 1);
-  }
+  const data = await getGithubContributesByMonth(
+    Array.from({ length: 12 }, () => {
+      const year = date.getFullYear();
+      const month = date.getMonth();
+      date = subMonths(date, 1);
+      return {
+        year,
+        month: month + 1,
+      };
+    })
+  );
 
-  const data = await Promise.all(promises);
-
-  const contributes = data
-    .map((amt, i) => ({
-      month: format(subMonths(new Date(), i), "yy MMM"),
-      amt,
-    }))
-    .reverse();
+  const contributes = Object.entries(data).map(([key, value]) => ({
+    month: format(parse(key, "MMMyy", new Date()), "yy MMM"),
+    amt: value,
+  })).reverse();
 
   return contributes;
 }
