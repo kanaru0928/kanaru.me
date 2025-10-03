866197cbfa662419d9cc66291ceee62ace5b70b6
kanaru0928 <kanaru0928@gmail.com>
2025-10-03 21:59:32 +0900

    feat: プロジェクトを初期化
    BREAKING CHANGE: 作り直し

--- infrastructure/modules/common/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-# resource "aws_cloudwatch_log_group" "log-group" {
-#   name              = "/aws/lambda/lambda-kanaru-me-app-${var.env}"
-#   retention_in_days = 30
-# }
--- infrastructure/modules/common/ssm.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ssm_parameter" "github_token" {
-  name  = "/kanaru.me/${var.env}/github_token"
-  type  = "SecureString"
-  value = "dummy"
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
