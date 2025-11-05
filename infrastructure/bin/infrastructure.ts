02cb215825b1dc7f8e2da65b592c7e2bfe00aca8
kanaru0928commit <kanaru0928@gmail.com>
2025-11-05 20:08:08 +0900

    feat: workflow を作成

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { config } from "dotenv";
+import { AppStack } from "../lib/app-stack";
 import { InfrastructureStack } from "../lib/infrastructure-stack";
 
 config({ path: `${__dirname}/../.env` });
 
 const app = new cdk.App();
-new InfrastructureStack(app, "InfrastructureStack", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+new InfrastructureStack(app, "InfrastructureStack", { env });
+new AppStack(app, "AppStack", { env });
