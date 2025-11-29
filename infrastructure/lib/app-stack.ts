fae72c1a7d6a312fe3a16cbd08b21f6b0c0895c6
kanaru0928commit <kanaru0928@gmail.com>
2025-11-29 20:01:15 +0900

    fix: prefix を修正

--- infrastructure/lib/app-stack.ts
+++ infrastructure/lib/app-stack.ts
@@ -135,7 +135,7 @@ export class AppStack extends cdk.Stack {
       environment: {
         DYNAMODB_TABLE_NAME: this.articleTable.tableName,
         S3_BUCKET_NAME: this.articleBucket.bucketName,
-        SECRET_NAME_PREFIX: `kanaru.me-v2/${this.environmentName}/`,
+        SECRET_NAME_PREFIX: `/kanaru.me-v2/${this.environmentName}/`,
         SSM_PARAMETER_STORE_TTL: "300",
       },
       initialPolicy: [
