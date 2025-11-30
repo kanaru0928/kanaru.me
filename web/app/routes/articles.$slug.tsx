5e1f031d6082b6a1e58bcae4bbc27023f3b42846
kanaru0928commit <kanaru0928@gmail.com>
2025-11-30 13:24:54 +0900

    fix: api 経由の表示でも rehype と remark を実施

--- web/app/routes/articles.$slug.tsx
+++ web/app/routes/articles.$slug.tsx
@@ -1,8 +1,11 @@
 import { evaluate } from "@mdx-js/mdx";
-import * as runtime from 'react/jsx-runtime';
+import * as runtime from "react/jsx-runtime";
 import { useLoaderData } from "react-router";
 import { apiClient } from "~/lib/apiClient";
 import type { Route } from "./+types/articles.$slug";
+import rehypeShiki from "@shikijs/rehype";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
 
 export async function loader({ params }: Route.LoaderArgs) {
   const { slug } = params;
@@ -21,9 +24,11 @@ export async function loader({ params }: Route.LoaderArgs) {
 export default async function ArticlesSlugRoute() {
   const { article } = useLoaderData<typeof loader>();
 
-  const {default: MDXContent} = await evaluate(article.contentBody, {
+  const { default: MDXContent } = await evaluate(article.contentBody, {
     ...runtime,
     development: false,
+    rehypePlugins: [[rehypeShiki, { theme: "catppuccin-mocha" }]],
+    remarkPlugins: [remarkFrontmatter, remarkGfm],
   });
 
   return <MDXContent />;
