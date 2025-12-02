f598ca098f36a14fc0baf808b59a1d8505e9fce5
kanaru0928commit <kanaru0928@gmail.com>
2025-12-02 09:46:24 +0900

    fix: 画像のレイアウトを調整

--- web/app/routes/articles.tsx
+++ web/app/routes/articles.tsx
@@ -3,14 +3,15 @@ import { cn } from "~/lib/utils";
 
 export default function ArticlesRoute() {
   return (
-    <div className="flex justify-center px-4 py-16">
+    <div className="flex justify-center px-8 py-16">
       <div
         className={cn(
-          "prose max-w-[600px]",
+          "prose lg:max-w-[600px]",
           "prose-h1:mb-8",
           "prose-h2:border-b-2 prose-h2:border-b-primary prose-h2:pb-1.5 prose-h1:text-4xl prose-h2:before:mr-3 prose-h2:before:text-primary/60 prose-h2:before:text-xl prose-h2:before:content-['##']",
           "prose-h3:before:mr-2 prose-h3:before:text-lg prose-h3:before:text-secondary-content/60 prose-h3:before:content-['###']",
           "prose-h4:before:mr-2 prose-h4:before:text-secondary-content/60 prose-h4:before:text-sm prose-h4:before:content-['####']",
+          "prose-img:block prose-img:h-auto prose-img:w-full has-prose-img:max-w-full"
         )}
       >
         <Outlet />
